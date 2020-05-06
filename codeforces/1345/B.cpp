d1a1ebc1844ef4209ba91fb888ce31aa2dbef3b3
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-06 21:43:00 +0600

    Add solution for problem `B - Card Constructions` Link: https://codeforces.com/contest/1345/problem/B Tags: binary search, brute force, dp, math, *1100 Ref: https://codeforces.com/contest/1345/submission/79187956

--- codeforces/1345/B.cpp
+++ codeforces/1345/B.cpp
@@ -1,75 +1,71 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 06 05 2020
 
+#include <bits/stdc++.h>
 
-    //...Bismillahir Rahmanir Rahim...
-    // Code by    Asad Bin Saber
-    // 06 05 2020
-     
-    #include <bits/stdc++.h>
-     
-    using namespace std;
-     
-    // typedefs...
-    typedef long long ll;
-    typedef unsigned long long ull;
-    typedef pair<int, int> pii;
-    typedef pair<int, pii> piii;
-    typedef vector<int> vi;
-    typedef vector<ll> vl;
-    typedef pair<ll, ll> pll;
-     
-    // constants...
-    const double PI = acos(-1);
-     
-    // defines...
-    #define MP make_pair
-    #define PB push_back
-    #define gcd(a,b) __gcd(a,b)
-    #define lcm(a,b) (a*(b/gcd(a,b))
-    #define si(a) scanf("%d", &a)
-    #define sii(a, b) scanf("%d%d", &a, &b)
-    #define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
-    #define sl(a) scanf("%lld", &a)
-    #define sll(a, b) scanf("%lld%lld", &a, &b)
-    #define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
-     
-    void task()
-    {
-        //code here...
-     
-        int t, n;
-        vector<int> ara;
-        ara.PB(0); ara.PB(2); ara.PB(7);
-        for(int K=3; ara[K-1]<1e9; K++){
-            ara.PB(2*ara[K-1] + 3 - ara[K-2]);
-        }
-     
-        cin >> t;
-        while(t--){
-            cin >> n;
-     
-            int ans = 0;
-            while(1){
-                auto it = upper_bound(ara.begin(), ara.end(), n);
-                --it;
-                if(*it==1 || *it==0) break;
-                n -= *it;
-                ans++;
-                //cout << *it << "\n";
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+void task()
+{
+    //code here...
+    ll t, n;
+
+    cin >> t;
+    while(t--){
+        cin >> n;
+
+        int ans = 0, mid, card;
+        while(1){
+            card = 0;
+            for(ll K=1; ; K++){
+                mid = (1+K)*K + K*(K-1)/2;
+                if(mid>n) break;
+                card = mid;
             }
-     
-            cout << ans << "\n";
+            if(n==1 || n==0) break;
+            n -= card;
+            ans++;
         }
+
+        cout << ans << "\n";
     }
-     
-    int main()
-    {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-     
-        //freopen("input.txt", "r", stdin);
-        //freopen("output.txt", "w", stdout);
-     
-        task();
-     
-        return 0;
-    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
