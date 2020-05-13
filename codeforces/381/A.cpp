1a47fef4257cd941e39e5a2e82d69b7ed02d8da0
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-13 16:28:00 +0600

    Add solution for problem `A - Sereja and Dima` Link: https://codeforces.com/contest/381/problem/A Tags: greedy, implementation, two pointers, *800 Ref: https://codeforces.com/contest/381/submission/79968440

---
+++ codeforces/381/A.cpp
@@ -0,0 +1,90 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 13 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+    int n, x, suj=0, dim=0;
+
+    cin >> n;
+    deque<int> ara;
+
+    for(int K=0; K<n; K++){
+        cin >> x;
+        ara.PB(x);
+    }
+
+    bool flag = 0;
+    while(!ara.empty()){
+        if(ara.front()>=ara.back()){
+            if(flag==0){
+                suj += ara.front();
+                ara.pop_front();
+                flag = 1;
+            }
+            else{
+                dim += ara.front();
+                ara.pop_front();
+                flag = 0;
+            }
+        }
+        else {
+            if(flag==0){
+                suj += ara.back();
+                ara.pop_back();
+                flag = 1;
+            }
+            else{
+                dim += ara.back();
+                ara.pop_back();
+                flag = 0;
+            }
+        }
+    }
+
+    cout << suj << ' ' << dim << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
