26fa0cf4be32a71812095ebb0e5a5638d0f0a0ac
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-14 17:38:00 +0600

    Add solution for problem `A - Is your horseshoe on the other hoof?` Link: https://codeforces.com/contest/228/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/228/submission/80064273

---
+++ codeforces/228/A.cpp
@@ -0,0 +1,61 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 14 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+    ll s[4], ans=0;
+    vector<bool> p(1000000001);
+
+    cin >> s[0] >> s[1] >> s[2] >> s[3];
+
+    for(int K=0; K<4; K++){
+        if(p[s[K]]==1) ans++;
+        else p[s[K]] = 1;
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
