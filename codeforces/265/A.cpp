adc1b4edfdd29e558f43d6ee30425f81f8cabced
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-15 12:12:00 +0600

    Add solution for problem `A - Colorful Stones (Simplified Edition)` Link: https://codeforces.com/contest/265/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/265/submission/80195585

---
+++ codeforces/265/A.cpp
@@ -0,0 +1,63 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 15 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+    string a, b;
+
+    cin >> a >> b;
+    int len_a = a.length();
+    int len_b = b.length();
+    int K, L;
+
+    for(K=0, L=0; K<len_a, L<len_b; L++){
+        if(a[K]==b[L]) K++;
+    }
+
+    cout << K+1 << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
