469af73359ee373d61cc89d41d90007c403c5421
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-15 14:32:00 +0600

    Add solution for problem `A - Carrot Cakes` Link: https://codeforces.com/contest/799/problem/A Tags: brute force, implementation, *1100 Ref: https://codeforces.com/contest/799/submission/80205394

---
+++ codeforces/799/A.cpp
@@ -0,0 +1,66 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 15 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+
+    int n, t, k, d;
+
+    cin >> n >> t >> k >> d;
+
+    int mid = n / k;
+    if(k*mid!=n) mid++;
+    int hi = (d/t);
+    if(t*hi!=d) hi++;
+    mid -= hi;
+    //cout << mid << ' ' << hi << "\n";
+
+    if(mid>1) cout << "YES\n";
+    else if(mid==1 && t*hi>d) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
