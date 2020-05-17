1c25f1467203e67da455427335d7c10688e55db6
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-17 16:31:00 +0600

    Add solution for problem `C1 - Simple Polygon Embedding` Link: https://codeforces.com/contest/1354/problem/C1 Tags: binary search, geometry, math, ternary search, *1400 Ref: https://codeforces.com/contest/1354/submission/80492968

---
+++ codeforces/1354/C1.cpp
@@ -0,0 +1,62 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 17 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+
+    int t, n;
+
+    cin >> t;
+    while(t--){
+        cin >> n;
+        n *= 2;
+
+        double x = PI/2 - PI / n, h;
+
+        h = 1.0 * tan(x);
+
+        printf("%.7lf\n", h);
+    }
+}
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
