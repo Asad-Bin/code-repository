da9d8f95a13b5dfef0d03df4268ad8306719b15e
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-19 16:22:00 +0600

    Add solution for problem `A - Pangram` Link: https://codeforces.com/contest/520/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/520/submission/80677006

---
+++ codeforces/520/A.cpp
@@ -0,0 +1,67 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 19 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+
+    int n;
+    string s;
+    set<char> ss;
+
+    cin >> n >> s;
+
+    for(int K=0; K<n; K++){
+        if(s[K]>='A' && s[K]<='Z'){
+            ss.insert(s[K]-'A'+'a');
+        } else{
+            ss.insert(s[K]);
+        }
+    }
+
+    if(ss.size()==26) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
