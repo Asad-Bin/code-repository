cf21d411d73d56d5ca5a22885c1c2b5d6a0a1fb8
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-20 21:15:00 +0600

    Add solution for problem `A - Panoramix's Prediction` Link: https://codeforces.com/contest/80/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/80/submission/80784723

---
+++ codeforces/80/A.cpp
@@ -0,0 +1,84 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 20 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+
+void task()
+{
+    //code here...
+    int n, m;
+
+    cin >> n >> m;
+
+    for(int K=n+1; K<m; K++){
+        bool flag = 0;
+        if(K%2==0){
+            flag=1;
+        }
+        for(int L=3; L*L<=K; L+=2){
+            if(K%L==0){
+                flag=1;
+            }
+        }
+
+        if(flag==0){
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    if(m%2==0){
+        cout << "NO\n";
+        return;
+    }
+    for(int K=3; K*K<=m; K+=2){
+        if(m%K==0){
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
