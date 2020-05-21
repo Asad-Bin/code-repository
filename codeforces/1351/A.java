5d648e036a670f0bad37d0e243f0271d3cb1b314
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-21 11:32:00 +0600

    Add solution for problem `A - A+B (Trial Problem)` Link: https://codeforces.com/contest/1351/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1351/submission/80822136

--- codeforces/1351/A.java
+++ codeforces/1351/A.java
@@ -1,19 +1,37 @@
+//package firsttest;
+
+import java.util.Date;
 import java.util.Scanner;
 
-public class Hello {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int i = 1, t = sc.nextInt();
-		
-		while(i<=t){
-			t--;
-			
-			int a = sc.nextInt();
-			
-			int b = sc.nextInt();
-			
-			System.out.println(a+b);
-		}
-	}
+/**
+ *
+ * @author Asad Bin Saber
+ * @date   12 05 2020
+ */
+public class Main{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Integers, doubles are base...
+        
+        int n, a, b, i;
+        
+        n = sc.nextInt();
+        for(i=1; i<=n; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a + b + "\n");
+        }
+        
+        //System.out.println("May I know your name?\n");
+        
+        //String name = sc.nextLine();
+        
+        //System.out.println("Hello " + name);
+    }
 }
