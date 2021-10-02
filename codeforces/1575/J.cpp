0e41f41df54bc62c807664c661de8a65a2bf044c
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-10-02 22:09:00 +0600

    Add solution for problem `J - Jeopardy of Dropped Balls` Link: https://codeforces.com/contest/1575/problem/J Tags: binary search, brute force, dsu, implementation, *1500 Ref: https://codeforces.com/contest/1575/submission/130581330

--- codeforces/1575/J.cpp
+++ codeforces/1575/J.cpp
@@ -1,121 +1,157 @@
+//...Bismillahir Rahmanir Rahim...
+// ***Asad Bin Saber***
+
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
-
+ 
 // typedefs...
-typedef double db;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<ll, ll> pll;
-typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oset;
-
+typedef long long            ll;
+typedef unsigned long long   ull;
+typedef pair<int, int>       pii;
+typedef vector<int>          vi;
+typedef vector<ll>           vl;
+typedef pair<ll, ll>         pll;
+ 
 // constants...
 const double PI = acos(-1);
-const ll mod =1000000007 ; // 998244353
-const int MXS = 2e5+5;
-const ll MXI = 1e9+5;
-const ll MXL = 1e18+5;
-const ll INF = 1e9+5;
-const ll INFLL = 1e18+5;
-const ll EPS = 1e-9;
+const int    mod = 1e9+7; // 998244353;
+const int    MXI = 1e9+5;
+const ll     MXL = 1e18+5;
 
 // defines...
-#define MP        make_pair
-#define PB        push_back
-#define fi         first
-#define se         second
-#define sz(x)      (int)x.size()
-#define all(x)      begin(x), end(x)
-#define si(a)       scanf("%d", &a)
-#define sii(a, b)    scanf("%d%d", &a, &b)
-#define ordered_set tree<ll,null_type,greater_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>
-#define boost_      ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-#define iter_(i,n)    for (int i = 0; i < int(n); i++)
-#define for_n(i, n)  for (int i = 1; i <= int(n); i++)
-#define print_array(a) for(int i=0;i<n;i++)  cout<<a[i]<<" ";
-#define rev(i,n)     for(int i=n;i>=0;i--)
-#define itr          ::iterator
-#define s_sort(s)    sort(s.begin(),s.end())
-#define n_sort(a, n)    sort(a,a+n)
-#define precise_impact cout<<setprecision(10)<<fixed;
-#define endl         "\n"
+#define MP             make_pair
+#define PB             push_back
+#define fi             first
+#define se             second
+#define sz(x)          (int)x.size()
+#define all(x)         begin(x), end(x)
+#define si(a)          scanf("%d", &a)
+#define sii(a, b)      scanf("%d%d", &a, &b)
+#define siii(a, b, c)  scanf("%d%d%d", &a, &b, &c)
+#define sl(a)          scanf("%lld", &a)
+#define sll(a, b)      scanf("%lld%lld", &a, &b)
+#define slll(a, b, c)  scanf("%lld%lld%lld", &a, &b, &c)
+#define precise_impact cout<<setprecision(6)<<fixed;
+
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair{ template <class T1, class T2> size_t operator()(const pair<T1, T2>& p) const{ auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2;}};
+
 // functions...
 ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
 ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
-ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
-ll bigmod(ll a,ll b){ if(b==0){ return 1;} ll tm=bigmod(a,b/2); tm=(tm*tm)%mod; if(b%2==1) tm=(tm*a)%mod; return tm;}
-ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
-ll modpow(ll a,ll p) {ll ans=1;while(p){if(p%2)ans=(ans*a)%mod;a=(a*a)%mod;p/=2;} return ans;}
-ll inverse_mod(ll n) {return modpow(n,mod-2);}
-//ll fact[2000005];
-//ll ncr_mod(ll n,ll r) {return (((fact[n]*inverse_mod(fact[r]))%mod)*inverse_mod(fact[n-r]))%mod;}
-int n,m,q;
-int a[1005][1005],c[1005];
-int check(int x,int y)
+ll bigmod(ll a, ll p, ll M)
 {
-    while(x!=n)
-    {
-        if(a[x][y]==0)  
-        {
-            a[x][y]=0;
-            return y;
-        }
-        else if(a[x][y]==2) ++x;
-        else if(a[x][y]==1)
-        {
-            a[x][y]=2;
-            c[y]++;
-            ++y;
-        }
-        else
-        {
-            a[x][y]=2;
-            c[y]++;
-            --y;
-        }
-    }
-    int i=n-1,ans=y;
-    while(i>=0&&a[i][y]==2)
-    {
-        a[i][y]=0;
-        --i;
-    }
-    return y;
+	// If 'M' is a prime number, then we can speed up it just by calculating "a^(p%(M-1))";
+	p %= (M-1);
+	ll result = 1LL;
+	while(p>0LL){
+		if(p&1LL) result = result * a % M;
+		a = a * a % M;
+		p>>=1LL;
+	}
+	return result;
 }
-void solve()
+ll inverse(ll a, ll M){
+	if(gcd(a, M)==1) return bigmod(a, M-2, M) % M; // (ax)%M = 1
+	return 1;
+}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K % mod; ans = ans * inverse(L, mod) % mod;} return ans;}
+ll egcd(ll a,ll b,ll &x,ll &y){
+	if(a==0){
+		x=0; y=1;
+		return b;
+	}
+	ll x1,y1;
+	ll d=egcd(b%a,a,x1,y1);
+	x=y1-(b/a)*x1; y=x1;
+	return d;
+}
+void pbin(int n)
 {
-    cin>>n>>m>>q;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            cin>>a[i][j];
-            if(a[i][j]==2) c[j]++;
-        }
-    }
-    for(int i=0;i<q;i++)
-    {
-       int temp;
-       cin>>temp;
-       if(c[temp-1]==n) cout<<temp<<" ";
-       else cout<<check(0,temp-1)+1<<" ";
-    }
+	vector<int> vv;
+	while(n){
+		vv.PB(n%2);
+		n >>= 1;
+	}
+	for(int K=sz(vv); K<=5; K++) vv.PB(0);
+	reverse(all(vv));
+	for(auto x:vv) cout << x;
+	cout << "\n";
 }
+
+
+const int MX = 1e3;
+int ara[MX+5][MX+5], col[MX+5];
+void task()
+{
+	// Code here.. ..
+	
+	int n, m, k, x;
+	
+	cin >> n >> m >> k;
+	
+	for(int K=1; K<=n; K++){
+		for(int L=1; L<=m; L++){
+			cin >> ara[K][L];
+			if(ara[K][L] == 2) col[L]++;
+		}
+	}
+	
+	while(k--){
+		cin >> x;
+		
+		if(col[x] == n){
+			cout << x << ' ';
+			continue;
+		}
+		
+		int a = 1, b = x;
+		while(a<=n){
+			if(ara[a][b] == 2){
+				a++;
+			}
+			else if(ara[a][b] == 1){
+				ara[a][b] = 2;
+				col[b]++;
+				b++;
+			}
+			else{
+				ara[a][b] = 2;
+				col[b]++;
+				b--;
+			}
+		}
+		
+		cout << b << ' ';
+	}
+	
+	cout << "\n";
+}
+
 int main()
 {
-    boost_;
-    int t=1;
-    //cin>>t;
-    for(int i=1;i<=t;i++)
-    {
-        
-        solve();
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    task();
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
