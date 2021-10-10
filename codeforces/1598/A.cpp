80adf6c928cfd154196af0bdb942a7ec5d3c43e8
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-10-10 15:10:00 +0600

    Add solution for problem `A - Computer Game` Link: https://codeforces.com/contest/1598/problem/A Tags: brute force, dfs and similar, dp, implementation, *800 Ref: https://codeforces.com/contest/1598/submission/131401142

---
+++ codeforces/1598/A.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+int main()
+{
+	#ifdef LOCAL
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+	
+	int t, n;
+	string s[2];
+	
+	cin >> t;
+	while(t--){
+		cin >> n >> s[0] >> s[1];
+		
+		bool ok = 1;
+		for(int K=0; K<n; K++){
+			if(s[0][K] == s[1][K] && s[0][K] == '1'){
+				ok = 0;
+				break;
+			}
+		}
+		
+		if(ok) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
