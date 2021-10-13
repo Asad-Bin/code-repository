c89132b369d461159569869d3057f8e229eecc62
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-10-13 20:52:00 +0600

    Add solution for problem `B - Make it Divisible by 25` Link: https://codeforces.com/contest/1593/problem/B Tags: dfs and similar, dp, greedy, math, math, *900 Ref: https://codeforces.com/contest/1593/submission/131767523

---
+++ codeforces/1593/B.cpp
@@ -0,0 +1,158 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+int main()
+{
+	#ifdef LOCAL
+        freopen("in.txt","r",stdin);
+        //freopen("out.txt","w",stdout);
+    #endif
+	
+    ll t, n;
+    vi v;
+    
+    sl(t);
+    while(t--){
+        sl(n);
+        
+        v.clear();
+        while(n){
+            v.PB(n%10);
+            n /= 10;
+        }
+        
+        reverse(v.begin(), v.end());
+        
+        bool f = 0;
+        int ans = 0;
+        for(int K=sz(v)-1; K>=0; K--){
+            if(v[K] == 0){
+                if(f){
+                    break;
+                }
+                else{
+                    f = 1;
+                    continue;
+                }
+            }
+            else if(v[K] == 5 && f){
+                break;
+            }
+            ans++;
+        }
+        
+        f = 0;
+        int ans1 = 0;
+        for(int K=sz(v)-1; K>=0; K--){
+            if(v[K] == 5 && !f){
+                f = 1;
+                continue;
+            }
+            else if((v[K] == 2 || v[K] == 7) && f){
+                break;
+            }
+            ans1++;
+        }
+        
+        printf("%d\n", min(ans, ans1));
+    }
+	
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
