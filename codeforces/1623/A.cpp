99298a6a92626087fd2f980b3f600bc874631b37
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-12-28 19:47:00 +0600

    Add solution for problem `A - Robot Cleaner` Link: https://codeforces.com/contest/1623/problem/A Tags: brute force, implementation, math, *800 Ref: https://codeforces.com/contest/1623/submission/140905347

---
+++ codeforces/1623/A.cpp
@@ -0,0 +1,138 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+int main()
+{
+	#ifdef LOCAL
+        freopen("in.txt","r",stdin);
+        //freopen("out.txt","w",stdout);
+    #endif
+	
+    int t, n, m, x, y, p, q;
+    
+    si(t);
+    
+    while(t--){
+        sii(n, m);
+        sii(x, y);
+        sii(p, q);
+        
+        int dx = 1, dy = 1;
+        int ans = 0;
+        while(true){
+            if(x == p || y == q) break;
+            
+            if(x+dx > n || x+dx < 1) dx *= -1;
+            if(y+dy > m || y+dy < 1) dy *= -1;
+            
+            x += dx;
+            y += dy;
+            
+            ans++;
+        }
+        
+        printf("%d\n", ans);
+    }
+    
+    
+    
+	
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
