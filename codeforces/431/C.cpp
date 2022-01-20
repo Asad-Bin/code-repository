b303ad5f65f7e9096fdf812455384ba6e740e154
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-01-20 20:39:00 +0600

    Add solution for problem `C - k-Tree` Link: https://codeforces.com/contest/431/problem/C Tags: dp, implementation, trees, *1600 Ref: https://codeforces.com/contest/431/submission/143433116

---
+++ codeforces/431/C.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+const int mod = 1e9+7;
+const int MX = 100;
+int k, d;
+int dp[MX+5][2];
+int calc(int num, bool ok)
+{
+    // if(at==k){
+        // if(num == 0 && ok) return 1;
+        // else return 0;
+    // }
+    if(num == 0){
+        if(ok) return 1;
+        else return 0;
+    }
+    
+    if(dp[num][ok] != -1) return dp[num][ok];
+    
+    int ans = 0;
+    for(int K = 1; K <= k; K++){
+        if(K > num) return dp[num][ok] = ans;
+        ans = (ans+calc(num-K, (K>=d ? ok|1 : ok)))%mod;
+    }
+    
+    return dp[num][ok] = ans;
+}
+int main()
+{
+	#ifdef LOCAL
+        freopen("in.txt","r",stdin);
+        //freopen("out.txt","w",stdout);
+    #endif
+	
+    int n;
+    
+    cin >> n >> k >> d;
+    
+    memset(dp, -1, sizeof dp);
+    cout << calc(n, 0) << "\n";;
+	
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
