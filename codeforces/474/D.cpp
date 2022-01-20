df0d73e218b05fb83edec4f4844a834696d91b19
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-01-20 21:09:00 +0600

    Add solution for problem `D - Flowers` Link: https://codeforces.com/contest/474/problem/D Tags: dp, *1700 Ref: https://codeforces.com/contest/474/submission/143436218

---
+++ codeforces/474/D.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+const int MX = 1e5;
+const int mod = 1e9+7;
+int dp[MX+5];
+int main()
+{
+	#ifdef LOCAL
+        freopen("in.txt","r",stdin);
+        //freopen("out.txt","w",stdout);
+    #endif
+	
+    
+    int t, k, a, b;
+    
+    cin >> t >> k;
+    
+    memset(dp, 0, sizeof dp);
+    for(int K = 1; K < k; K++) dp[K] = 1;
+    
+    dp[k] = 2;
+    int i = 1;
+    for(int K = k+1; K <= MX; K++){
+        dp[K] = (dp[K-1]+dp[K-k])%mod;
+    }
+    
+    for(int K = 2; K <= MX; K++) dp[K] = (dp[K]+dp[K-1])%mod;
+    
+    while(t--){
+        cin >> a >> b;
+        
+        cout << (dp[b]-dp[a-1]+mod)%mod << "\n";
+    }
+	
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
