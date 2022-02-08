8b4d3b30463c3a402b842f846e0483a613a0250f
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-02-08 13:31:00 +0600

    Add solution for problem `A - Park Lighting` Link: https://codeforces.com/contest/1358/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1358/submission/145616508

--- codeforces/1358/A.cpp
+++ codeforces/1358/A.cpp
@@ -1,66 +1,66 @@
-//...Bismillahir Rahmanir Rahim...
-// Code by    Asad Bin Saber
-// 26 05 2020
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-// typedefs...
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<int, pii> piii;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<ll, ll> pll;
-
-// constants...
-const double PI = acos(-1);
-
-// defines...
-#define MP make_pair
-#define PB push_back
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*(b/gcd(a,b))
-#define si(a) scanf("%d", &a)
-#define sii(a, b) scanf("%d%d", &a, &b)
-#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
-#define sl(a) scanf("%lld", &a)
-#define sll(a, b) scanf("%lld%lld", &a, &b)
-#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
-
-void task()
-{
-    //code here...
-
-    int t, n, m;
-
-    cin >> t;
-    while(t--){
-        cin >> n >> m;
-
-        int ans = max(n,m)/2;
-        ans *= min(n, m);
-        if(max(n, m)%2==1){
-            ans += (min(n, m)+1)/2;
-
+    //...Bismillahir Rahmanir Rahim...
+    // Code by    Asad Bin Saber
+    // 26 05 2020
+     
+    #include <bits/stdc++.h>
+     
+    using namespace std;
+     
+    // typedefs...
+    typedef long long ll;
+    typedef unsigned long long ull;
+    typedef pair<int, int> pii;
+    typedef pair<int, pii> piii;
+    typedef vector<int> vi;
+    typedef vector<ll> vl;
+    typedef pair<ll, ll> pll;
+     
+    // constants...
+    const double PI = acos(-1);
+     
+    // defines...
+    #define MP make_pair
+    #define PB push_back
+    #define gcd(a,b) __gcd(a,b)
+    #define lcm(a,b) (a*(b/gcd(a,b))
+    #define si(a) scanf("%d", &a)
+    #define sii(a, b) scanf("%d%d", &a, &b)
+    #define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+    #define sl(a) scanf("%lld", &a)
+    #define sll(a, b) scanf("%lld%lld", &a, &b)
+    #define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+     
+    void task()
+    {
+        //code here...
+     
+        int t, n, m;
+     
+        cin >> t;
+        while(t--){
+            cin >> n >> m;
+     
+            int ans = max(n,m)/2;
+            ans *= min(n, m);
+            if(max(n, m)%2==1){
+                ans += (min(n, m)+1)/2;
+     
+            }
+     
+            cout << ans << "\n";
         }
-
-        cout << ans << "\n";
     }
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-
-    task();
-
-    return 0;
-}
-
+     
+    int main()
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+     
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+     
+        task();
+     
+        return 0;
+    }
+     
