2cdbaeda1a57d5446547eb622e771d5f6a5bc1f6
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-02-09 20:18:00 +0600

    Add solution for problem `C - The Football Season` Link: https://codeforces.com/contest/1244/problem/C Tags: brute force, math, number theory, *2000 Ref: https://codeforces.com/contest/1244/submission/145761232

--- codeforces/1244/C.cpp
+++ codeforces/1244/C.cpp
@@ -7,6 +7,15 @@
 using namespace std;
 
 
+long long gcd(long long a, long long b) {
+   if (b == 0)
+   return a;
+   return gcd(b, a % b);
+}
+long long lcm(long long a, long long b)
+{
+	return a/gcd(a, b)*b;
+}
 int main()
 {
 	long long n, p, w, d;
@@ -22,6 +31,36 @@ int main()
 			return 0;
 		}
 	}
+	
+	//long long x = w*d; //lcm(w, d);
+	
+	//long long ans = p / x;
+	//ans *= (x/w);
+	//p %= x;
+	
+	////cout << x << ' ' << ans << ' ' << p << "\n";
+	
+	//while(ans >= 0){
+		//long long i = 0;
+		////cout << i+ans+(p - i*w)/d << "\n";
+		//while(i*w <= p && i+ans <= n){
+			//long long mid = p - i*w;
+			
+			////if(i+ans+mid/d > n) continue;
+			
+			////cout << "hi\n";
+			////cout << i*w << ' ' << mid << "\n";
+			//if(mid%d == 0 && i+ans+mid/d <= n){
+				//cout << i+ans << ' ' << mid/d << ' ' << n-i-ans-(mid/d);
+				//return 0;
+			//}
+			
+			//i++;
+		//}
+		
+		//ans -= x/w;
+		//p += x;
+	//}
 	
 	cout << "-1\n";
 	
