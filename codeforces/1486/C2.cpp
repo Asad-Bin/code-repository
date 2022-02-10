93833bc30fd7ef32438c86f67881e442462fbd9d
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-02-10 16:11:00 +0600

    Add solution for problem `C2 - Guessing the Greatest (hard version)` Link: https://codeforces.com/contest/1486/problem/C2 Tags: binary search, interactive, *1900 Ref: https://codeforces.com/contest/1486/submission/145836100

--- codeforces/1486/C2.cpp
+++ codeforces/1486/C2.cpp
@@ -63,5 +63,118 @@ int main()
 	}
 	
 	
+	
+	//int l = 1, r = n;
+	//while(l < r){
+		//if(r-l+1 == 2){
+			//x = num;
+			
+			//cout << '!' << ' ' << (x==l ? r : l) << endl;
+			//break;
+		//}
+		
+		//else if(r-l+1 == 3){
+			//int a, b, c;
+			//a = b = c = 0;
+			
+			//x = num;
+			
+			//if(x == l) a = -1;
+			//else if(x == r) c = -1;
+			//else b = -1;
+			
+			//cout << '?' << ' ' << l << ' ' << l+1 << endl;
+			//cin >> x;
+			
+			//if(x == l) a = -1;
+			//else b = -1;
+			
+			////if(a==-1 && b==-1){
+				////cout << '!' << ' ' << r << "\n";
+				////break;
+			////}
+			////else if(a==-1 && c==-1){
+				////cout << '!' << ' ' << l+1 << "\n";
+				////break;
+			////}
+			////else if(b==-1 && c==-1){
+				////cout << '!' << ' ' << l << "\n";
+				////break;
+			////}
+			
+			//cout << '?' << ' ' << l+1 << ' ' << r << endl;
+			//cin >> x;
+			
+			//if(x == r) c = -1;
+			//else b = -1;
+			
+			//if(a == 0) cout << '!' << ' ' << l << "\n";
+			//else if(b == 0) cout << '!' << ' ' << l+1 << "\n";
+			//else cout << '!' << ' ' << r << "\n";
+			
+			//break;
+		//}
+		
+		//int mid = (l+r)/2;
+		
+		
+		//if(num <= mid){
+			//cout << '?' << ' ' << l << ' ' << mid << endl;
+			//cin >> x;
+			
+			//if(num == x){
+				//r = mid;
+				//continue;
+			//}
+			//else{
+				//cout << '?' << ' ' << mid+1 << ' ' << r << endl;
+				//cin >> y;
+				
+				//l = mid+1;
+				//num = y;
+			//}
+		//}
+		//else{
+			//cout << '?' << ' ' << mid+1 << ' ' << r << endl;
+			//cin >> y;
+			
+			//if(num == y){
+				//l = mid+1;
+			//}
+			//else{
+				//cout << '?' << ' ' << l << ' ' << mid << endl;
+				//cin >> x;
+				
+				//r = mid;
+				//num = x;
+			//}
+		//}
+		
+			////cout << '?' << ' ' << l << ' ' << mid << endl;
+			////cin >> x;
+		
+		////if(num == x){
+			////r = mid;
+			////continue;
+		////}
+		
+			////cout << '?' << ' ' << mid+1 << ' ' << r << endl;
+			////cin >> y;
+		
+		////if(num == y){
+			////l = mid+1;
+		////}
+		////else{
+			////if(num <= mid){
+				////l = mid+1;
+				////num = y;
+			////}
+			////else{
+				////r = mid;
+				////num = x;
+			////}
+		////}
+	//}
+	
 	return 0;
 }
