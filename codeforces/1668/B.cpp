26f65e0a3baf72ee491cbc634fe29b254449688d
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-04-19 21:56:00 +0600

    Add solution for problem `B - Social Distance` Link: https://codeforces.com/contest/1668/problem/B Tags: greedy, math, sortings, *900 Ref: https://codeforces.com/contest/1668/submission/154116375

---
+++ codeforces/1668/B.cpp
@@ -0,0 +1,141 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+const int MX = 1e5;
+ll ara[MX+5];
+int main()
+{
+	ll t, n, m;
+	
+	cin >> t;
+	while(t--){
+		cin >> n >> m;
+		
+		for(int K = 0; K < n; K++) cin >> ara[K];
+		
+		sort(ara, ara+n);
+		
+		bool ok = 1;
+		ll at = ara[0]+1, pre = ara[0];
+		
+		for(int K = 1; K < n; K++){
+			pre = max(pre, ara[K]);
+			
+			at += pre+1;
+			
+			//cout << K << ' ' << ara[K] << ' ' << pre << ' ' << at << "\n";
+			pre = ara[K];
+		}
+		
+		if(at > m) ok = 0;
+		//pre = max(pre, ara[0]);
+		at += pre;
+		if(at > m+ara[0]) ok = 0;
+		//cout << at << ' ' << pre << "\n";
+		
+		if(ok) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
