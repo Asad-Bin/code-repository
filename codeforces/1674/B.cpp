41d09fa732ff76025b08a08a6683967ba48a8dc2
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-05-02 20:42:00 +0600

    Add solution for problem `B - Dictionary` Link: https://codeforces.com/contest/1674/problem/B Tags: combinatorics, math Ref: https://codeforces.com/contest/1674/submission/155598614

---
+++ codeforces/1674/B.cpp
@@ -0,0 +1,132 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+int main()
+{
+	map<string, int> mp;
+	int i = 1;
+	string s;
+	
+	for(int K = 0; K < 26; K++){
+		s = "aa";
+		s[0] = 'a'+K;
+		for(int L = 0; L < 26; L++){
+			if(K != L){
+				s[1] = 'a'+L;
+				
+				mp[s] = i++;
+			}
+		}
+	}
+	
+	int t;
+	cin >> t;
+	while(t--){
+		cin >> s;
+		
+		cout << mp[s] << "\n";
+	}
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
